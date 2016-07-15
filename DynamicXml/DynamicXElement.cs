74778cd044949dec3ea141292dad7a70e6324118
elie <eliea@code-college.net>
2016-07-16 00:09:37 +0300

    Add project files.

---
+++ DynamicXml/DynamicXElement.cs
@@ -0,0 +1,45 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DynamicXml
+{
+    class DynamicXElement : DynamicObject
+    {
+        private readonly XElement _element;
+
+        private DynamicXElement(XElement xelement)
+        {
+            _element = xelement;
+        }
+
+        public static dynamic Create(XElement xelement)
+        {
+            var dynCreate =  new DynamicXElement(xelement);
+            return dynCreate;
+        }
+
+        public override bool TryGetMember(GetMemberBinder memberBinder, out object result)
+        {
+            result = new DynamicXElement(_element.Element(memberBinder.Name));
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder indexBinder, object[] indexes, out object result)
+        {
+            var name = (string)indexes[0];
+            var index = (int)indexes[1];
+
+            if (name.GetType() != typeof(string) || index.GetType() != typeof(int) || indexes.Length != 2)
+            {
+                result = null;
+                return false;
+            }
+
+            var indexedElements = _element.Elements(name).ToList();
+            result = new DynamicXElement(indexedElements.ElementAt(index));
+            return true;
+        }
+        public override string ToString() => _element.Value;
+    }
+}
---
+++ DynamicXml/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DynamicXml
+{
+    class Program
+    {
+        static void Main()
+        {
+            dynamic planets = DynamicXElement.Create(XElement.Load("Planets.xml"));
+            var mercury = planets.Planet;
+            var venus = planets["Planet", 1];
+            var ourMoon = planets["Planet", 2].Moons.Moon;
+            var marsMoon = planets["Planet", 3]["Moons", 0].Moon;
+
+            Console.WriteLine(mercury);
+            Console.WriteLine(venus);
+            Console.WriteLine(ourMoon);
+            Console.WriteLine(marsMoon);
+        }
+    }
+}
